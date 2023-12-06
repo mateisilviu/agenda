04d1176061056daa96692959dc4b2dbf695d5113
Silviu Matei <tzevy.cont@gmail.com>
2023-12-06 14:22:11 +0200

    adding contact page

---
+++ src/main/java/com/example/agenda/AddContact.java
@@ -0,0 +1,14 @@
+package com.example.agenda;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AddContact {
+
+    String nume;
+    String email;
+    String telefon;
+
+}
--- src/main/java/com/example/agenda/ContactController.java
+++ src/main/java/com/example/agenda/ContactController.java
@@ -2,12 +2,15 @@ package com.example.agenda;
 
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 @RequiredArgsConstructor
@@ -20,6 +23,19 @@ public class ContactController {
         var contacte = contactRepository.findAll();
         model.addAttribute("contacts", contacte);
         return "index";
+    }
+
+    @GetMapping("/add")
+    public String addContact() {
+        return "add";
+    }
+
+    @PostMapping("/add")
+    public String doAddContact(AddContact entity) {
+        Contact c = new Contact();
+        BeanUtils.copyProperties(entity, c);
+        contactRepository.save(c);
+        return "redirect:/";
     }
 
 }
