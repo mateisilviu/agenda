b80a94b0bac3d09eb84c33efbdbc7050241d33a4
Silviu Matei <tzevy.cont@gmail.com>
2023-12-06 15:03:36 +0200

    bean validation example

--- src/main/java/com/example/agenda/ContactController.java
+++ src/main/java/com/example/agenda/ContactController.java
@@ -5,9 +5,12 @@ import java.util.List;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -45,8 +48,15 @@ public class ContactController {
     }
 
     @PostMapping("/edit")
-    public String editContact(@RequestBody Contact entity, Model model) {
-        contactRepository.save(entity);
+    public String editContact(@Valid @ModelAttribute("contact") UpdateContact entity, BindingResult result,
+            Model model) {
+        if (result.hasErrors()) {
+            model.addAttribute("eroare", "Date invalide");
+            return "edit";
+        }
+        Contact c = new Contact();
+        BeanUtils.copyProperties(entity, c);
+        contactRepository.save(c);
         // return "redirect:/edit?id=" + entity.getId();
         return "redirect:/";
     }
